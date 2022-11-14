b833d582343e76c5b06544c66a73a52d50be99f2
tarr4h <81558152+tarr4h@users.noreply.github.com>
2022-11-14 21:08:31 +0900

    commit

--- src/main/java/com/egov/voc/kspo/setting/service/VocTaskCodeSettingService.java
+++ src/main/java/com/egov/voc/kspo/setting/service/VocTaskCodeSettingService.java
@@ -10,8 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
