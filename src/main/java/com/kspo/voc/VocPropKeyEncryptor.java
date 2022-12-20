a64405141ab5637043594ef90fa71611f1c88f76
stkim <stkim@wigoai.com>
2022-12-20 14:19:12 +0900

    프로퍼티 암호화

---
+++ src/main/java/com/kspo/voc/VocPropKeyEncryptor.java
@@ -0,0 +1,16 @@
+package com.kspo.voc;
+
+import org.jasypt.encryption.StringEncryptor;
+
+import com.kspo.voc.comn.config.JasyptConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class VocPropKeyEncryptor {
+//	public static void main(String[] args) {
+//		StringEncryptor encryptor = new JasyptConfiguration().stringEncryptor();
+//		String value = "ABCDEFG";
+//		log.info(value + " ==> ENC(" + encryptor.encrypt(value) + ")");
+//	}
+}
