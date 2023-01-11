6c17a3d4b0a9a0b74a6db3e11115fb191e35cfc0
김성태 <stkim@wigoai.com>
2023-01-11 15:43:06 +0900

    pmd

--- src/main/java/com/kspo/voc/comn/taglib/FileInfoTag.java
+++ src/main/java/com/kspo/voc/comn/taglib/FileInfoTag.java
@@ -1,7 +1,5 @@
 package com.kspo.voc.comn.taglib;
 
-
-
 import java.io.IOException;
 import java.util.List;
 
@@ -12,6 +10,8 @@ import com.kspo.base.common.model.EzMap;
 import com.kspo.voc.comn.util.Utilities;
 import com.kspo.voc.sys.model.FileBaseVo;
 import com.kspo.voc.sys.service.FileService;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
@@ -26,7 +26,7 @@ import com.kspo.voc.sys.service.FileService;
  * @Version : 1.0
  * @Company : Copyright ⓒ wigo.ai. All Right Reserved
  */
-
+@Slf4j
 public class FileInfoTag extends TagSupport {
 
 	/**
@@ -37,8 +37,8 @@ public class FileInfoTag extends TagSupport {
 	private String name;
 	private String fileId;
 	private int fileSeq = 0;
-	private String acceptFileTypes="";
-	private String acceptExts="";
+	private String acceptFileTypes = "";
+	private String acceptExts = "";
 	private boolean viewMode = false;
 	private String fileCategory = "attachment";
 	FileService service = Utilities.getBean(FileService.class);
@@ -47,17 +47,23 @@ public class FileInfoTag extends TagSupport {
 	public int doStartTag() throws JspException {
 		StringBuffer html = new StringBuffer();
 		StringBuffer idNm = new StringBuffer();
-		if(Utilities.isNotEmpty(id))
-			idNm.append(" id='").append(id).append( "'");
-		if(Utilities.isNotEmpty(name))
-			idNm.append(" name='").append( name).append( "'");
-		String dataInfo = "data-file-id='"+fileId+"' data-file-seq='"+fileSeq+"' data-file-category='"+fileCategory+"' data-accept-ext='"+acceptExts+"' data-accept-types='"+acceptFileTypes+"' "+idNm;
+		if (Utilities.isNotEmpty(id))
+			idNm.append(" id='").append(id).append("'");
+		if (Utilities.isNotEmpty(name))
+			idNm.append(" name='").append(name).append("'");
+		String dataInfo = "data-file-id='" + fileId + "' data-file-seq='" + fileSeq + "' data-file-category='"
+				+ fileCategory + "' data-accept-ext='" + acceptExts + "' data-accept-types='" + acceptFileTypes + "' "
+				+ idNm;
 		StringBuffer info = new StringBuffer();
-		info.append("<span " ).append( dataInfo ).append( " data-file-info='info' >");
-		String button = "&nbsp;<button "+dataInfo+" class=\"btnInner gray btnSubmit\" type=\"button\" data-click=\"onFileInfo\" data-file-info='button'>첨부</button>";
-		String cancel = "&nbsp;<button "+dataInfo+" class=\"btnInner gray btnSubmit\" style='display:none'  type=\"button\" data-click=\"onCancelFileInfo\"  data-file-info='cancel'>취소</button>";
-		String remove = "&nbsp;<button "+dataInfo+" class=\"btnInner gray btnSubmit\" style='display:none'  type=\"button\" data-click=\"onRemoveFileInfo\"  data-file-info='remove'>삭제</button>";
-		String download = "&nbsp;<button "+dataInfo+" style='display:none' class=\"btnInner gray btnSubmit\" data-file-url='' type=\"button\" data-click=\"onDownloadFileInfo\"  data-file-info='download'>받기</button>";
+		info.append("<span ").append(dataInfo).append(" data-file-info='info' >");
+		String button = "&nbsp;<button " + dataInfo
+				+ " class=\"btnInner gray btnSubmit\" type=\"button\" data-click=\"onFileInfo\" data-file-info='button'>첨부</button>";
+		String cancel = "&nbsp;<button " + dataInfo
+				+ " class=\"btnInner gray btnSubmit\" style='display:none'  type=\"button\" data-click=\"onCancelFileInfo\"  data-file-info='cancel'>취소</button>";
+		String remove = "&nbsp;<button " + dataInfo
+				+ " class=\"btnInner gray btnSubmit\" style='display:none'  type=\"button\" data-click=\"onRemoveFileInfo\"  data-file-info='remove'>삭제</button>";
+		String download = "&nbsp;<button " + dataInfo
+				+ " style='display:none' class=\"btnInner gray btnSubmit\" data-file-url='' type=\"button\" data-click=\"onDownloadFileInfo\"  data-file-info='download'>받기</button>";
 
 		EzMap param = new EzMap();
 		param.setString("fileId", fileId);
@@ -68,36 +74,36 @@ public class FileInfoTag extends TagSupport {
 		try {
 			fileList = service.getList(param);
 			if (Utilities.isEmpty(fileList))
-				info .append( "첨부없음");
+				info.append("첨부없음");
 			else {
-				remove = "&nbsp;<button "+dataInfo+" class=\"btnInner gray btnSubmit\" type=\"button\" data-click=\"onRemoveFileInfo\"  data-file-info='remove'>삭제</button>";
-				download = "&nbsp;<button "+dataInfo+" class=\"btnInner gray btnSubmit\" data-file-url='"+fileList.get(0).getFileUrl()+"' data-file-nm='"+fileList.get(0).getFileNm()+"' type=\"button\" data-click=\"onDownloadFileInfo\"  data-file-info='download'>받기</button>";
-				info.append( fileList.get(0).getFileNm());
-				
+				remove = "&nbsp;<button " + dataInfo
+						+ " class=\"btnInner gray btnSubmit\" type=\"button\" data-click=\"onRemoveFileInfo\"  data-file-info='remove'>삭제</button>";
+				download = "&nbsp;<button " + dataInfo + " class=\"btnInner gray btnSubmit\" data-file-url='"
+						+ fileList.get(0).getFileUrl() + "' data-file-nm='" + fileList.get(0).getFileNm()
+						+ "' type=\"button\" data-click=\"onDownloadFileInfo\"  data-file-info='download'>받기</button>";
+				info.append(fileList.get(0).getFileNm());
+
 				if (fileList.size() > 1)
-					info.append( "(총 " ).append( fileList.size() ).append( " 파일)");
+					info.append("(총 ").append(fileList.size()).append(" 파일)");
 			}
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.warn(e.getMessage());
 			return SKIP_BODY;
 		}
-		info.append(  "</span>");
+		info.append("</span>");
 		html.append(info);
-		if (!viewMode)
-		{
+		if (!viewMode) {
 			html.append(button);
 			html.append(cancel);
 			html.append(download);
 			html.append(remove);
-			
-			
-		}
-		else {
+
+		} else {
 			if (Utilities.isNotEmpty(fileList))
 				html.append(download);
 		}
-		
+
 		try {
 			pageContext.getOut().print(html.toString());
 		} catch (IOException e) {
